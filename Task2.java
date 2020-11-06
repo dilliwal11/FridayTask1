aca5af43d510b11639bdd655c1b5b46f04757776
Akash <dilliwalakash@gmail.com>
2020-11-06 14:43:38 -0600

    online class task

---
+++ Task2.java
@@ -0,0 +1,16 @@
+class Task2 {
+    public static void main (String args[]){
+
+    for(int i = 4; i >=0; i--){
+
+    for(int j = i; j > 0; j--){
+    System.out.print(" ");
+    }
+
+    for(int k = 4-i; k >=0 ; k--)
+    System.out.print( k+1);
+
+ System.out.println();
+    }
+    }
+}
